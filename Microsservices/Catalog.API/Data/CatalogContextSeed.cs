9db674bf448bbdd3ed7cc0ecf1af48f751249874
M4theusVieir4 <vieiramatheus72@gmail.com>
2025-03-23 00:29:52 -0300

    Adicionando os arquivos do projeto Microserviços ao repositório eShopOnContainers

---
+++ Microsservices/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,54 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if (!existProduct) 
+            {
+                productCollection.InsertManyAsync(GetMyProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetMyProducts() 
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Id = "602d2149e773f2a3990b47f5",
+                    Name = "IPhone X",
+                    Description = "akdnsaiojdioasjdiojaidojasiodjkiasdjkioasjdioajiodjusaiodjusoiadasdsa",
+                },
+                new Product()
+                {
+                    Id = "602d2149e773f2a3990b47f5",
+                    Name = "IPhone X",
+                    Description = "akdnsaiojdioasjdiojaidojasiodjkiasdjkioasjdioajiodjusaiodjusoiadasdsa",
+                },
+                new Product()
+                {
+                    Id = "602d2149e773f2a3990b47f5",
+                    Name = "IPhone X",
+                    Description = "akdnsaiojdioasjdiojaidojasiodjkiasdjkioasjdioajiodjusaiodjusoiadasdsa",
+                },
+                new Product()
+                {
+                    Id = "602d2149e773f2a3990b47f5",
+                    Name = "IPhone X",
+                    Description = "akdnsaiojdioasjdiojaidojasiodjkiasdjkioasjdioajiodjusaiodjusoiadasdsa",
+                },
+                new Product()
+                {
+                    Id = "602d2149e773f2a3990b47f5",
+                    Name = "IPhone X",
+                    Description = "akdnsaiojdioasjdiojaidojasiodjkiasdjkioasjdioajiodjusaiodjusoiadasdsa",
+                }
+            };
+        }
+    }
+}
